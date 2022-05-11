372b2e5c1c42a8e99df2456ec487662f7d4a0c33
mnaofficial <mna.official92@gmail.com>
2022-05-11 22:52:18 +0600

    service api create

--- index.js
+++ index.js
@@ -14,10 +14,30 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('doctors_portal').collection('services')
+
+        //service api for find all data
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
-  res.send('Hello from Doctors portal!')
+    res.send('Hello from Doctors portal!')
 })
 
 app.listen(port, () => {
-  console.log(`Doctors portal app listening on port ${port}`)
+    console.log(`Doctors portal app listening on port ${port}`)
 })
