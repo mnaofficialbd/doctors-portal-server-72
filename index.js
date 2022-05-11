89e2db51796a965959ab3dc826448f7ebd3f1dd9
mnaofficial <mna.official92@gmail.com>
2022-05-11 21:43:43 +0600

    Connect with database

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express()
 const port = process.env.PORT || 5000
 
 //middleware
 app.use(cors());
 app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qbdaq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 app.get('/', (req, res) => {
   res.send('Hello from Doctors portal!')
